5e9ac508d52eca9579e43d7b5b99f8fa961a57cc
Alex <96828891+OleksandrZolotarov@users.noreply.github.com>
2025-08-12 16:34:34 +0800

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -60,22 +60,15 @@
         if (!target.classList.contains("_slide")) {
             target.classList.add("_slide");
             target.hidden = target.hidden ? false : null;
-            showmore ? target.style.removeProperty("height") : null;
-            let height = target.offsetHeight;
+            target.style.removeProperty("height");
+            let fullHeight = target.offsetHeight;
             target.style.overflow = "hidden";
-            target.style.height = showmore ? `${showmore}px` : `0px`;
-            target.style.paddingTop = 0;
-            target.style.paddingBottom = 0;
-            target.style.marginTop = 0;
-            target.style.marginBottom = 0;
+            let startHeight = showmore ? showmore : target.offsetHeight;
+            target.style.height = startHeight + "px";
             target.offsetHeight;
             target.style.transitionProperty = "height, margin, padding";
             target.style.transitionDuration = duration + "ms";
-            target.style.height = height + "px";
-            target.style.removeProperty("padding-top");
-            target.style.removeProperty("padding-bottom");
-            target.style.removeProperty("margin-top");
-            target.style.removeProperty("margin-bottom");
+            target.style.height = fullHeight + "px";
             window.setTimeout((() => {
                 target.style.removeProperty("height");
                 target.style.removeProperty("overflow");
